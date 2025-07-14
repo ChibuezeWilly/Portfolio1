43189624afe8daa8a89e07bebcd2fdb737c898aa
Chibueze Williams <chibuezewilliams606@gmail.com>
2025-07-14 11:25:56 +0100

    Added facebookmarketplace experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,10 +9,10 @@ const Experience = () => {
 		<div className="w-full h-auto pb-10 bg-neutral-900 mt-10" id="#experience">
 			<div className="pb-10 w-full px-5 lg:px-16 pt-5 ">
 				<h1 className="header font-bold text-3xl  text-white">Experience</h1>
-				<div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-10">
+				<div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-3">
 					{/* first */}
 					<div
-						className="mt-10 h-96 flex flex-row justify-evenly"
+						className="mt-10 h-auto flex flex-row justify-evenly"
 						data-aos="fade-right"
 						data-aos-delay="50"
 						data-aos-duration="1000"
@@ -47,7 +47,7 @@ const Experience = () => {
 					</div>
 					{/* second */}
 					<div
-						className=" mt-10 h-96 flex flex-row justify-evenly"
+						className=" mt-10 h-auto flex flex-row justify-evenly"
 						data-aos="fade-left"
 						data-aos-delay="50"
 						data-aos-duration="1000"
@@ -75,6 +75,44 @@ const Experience = () => {
 									<span className="header">useEffect</span>, and ensuring
 									responsive design with{" "}
 									<span className="header">Tailwind CSS</span>
+								</li>
+							</div>
+						</div>
+					</div>
+					{/* third */}
+					<div
+						className="h-auto flex flex-row justify-evenly mt-10"
+						data-aos="fade-left"
+						data-aos-delay="50"
+						data-aos-duration="1000"
+						data-aos-easing="ease-in-out"
+						data-aos-once="true"
+						data-aos-anchor-placement="top-center">
+						<div className="bg-gray-600 h-7 w-14 rounded-tr-xl flex justify-center items-center">
+							<p className="text-white">2025</p>
+						</div>
+						<div className="left w-full ml-2">
+							<h1 className="first text-white font-bold text-xl md:text-2xl ml-5 md:ml-10">
+								Frontend Developer
+							</h1>
+							<div className="second ml-6" style={{ fontFamily: "Poppins" }}>
+								<li className="text-gray-200 ml-5 text-wrap leading-relaxed tracking-wide text-sm mt-5">
+									I worked on a full-stack web application inspired by Facebook
+									Marketplace, allowing users to browse, post, and manage
+									listings for items for sale.{" "}
+								</li>
+								<li className="text-gray-200 ml-5 text-wrap leading-relaxed tracking-wide text-sm mt-5">
+									{" "}
+									Built with  {""}
+									 <span className="header">
+										Next.js 15, React.js, Typescript
+									</span>{" "}
+									for the front-end, implementing smooth state management using{" "}
+									<span className="header">Zustand</span> and{" "}
+									<span className="header">Tailwind CSS </span>
+									for styling {""}
+									 <span className="header">Cursor AI </span>
+									for debugging and speeding up development process
 								</li>
 							</div>
 						</div>
